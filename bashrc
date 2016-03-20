f88f7d2388e58221157dccb17d2dd017c2ca4892
PewMcDaddy <phil103@hotmail.com>
2016-03-20 01:55:16 -0400

    Completed install script for colors and indent

--- bashrc
+++ bashrc
@@ -14,34 +14,5 @@
 	# 	source $HOME/.bash_profile
     # fi
 echo "Sourcing bashrc"
-# Make bash history unlimited.
-export HISTFILESIZE=
-export HISTSIZE=
-#	From "man bash":
-#	
-#	    If HISTFILESIZE is not set, no truncation is performed.
 
-PS1='\[\e[0;32m\][\u@\h \W] \$ \[\e[0m\]'
-
-alias INF1995='firefox http://www.groupes.polymtl.ca/inf1995/tp/'
-alias docAtmel='gvfs-open ~/Documents/docAtmel.pdf'
-# Put the PDF of the Atmel documentation in the documents folder.
-
-alias docAVRLibC='firefox http://www.nongnu.org/avr-libc/user-manual/index.html'
-alias chrome='google-chrome'
-alias INF1995='google-chrome http://www.groupes.polymtl.ca/inf1995/tp/'
-alias hotmail='google-chrome www.hotmail.com'
-alias ssh-school='ssh l4712-05.info.polymtl.ca -l phcarb'
-
-#	# Lancer le TP4 a chaque fois qu'on ouvre un terminal 
-	nohup ~/Documents/FedoraInstall/INF1600_TP4/tp4 > /dev/null 2>&1 &
-#	# nohup <programme> > /def/null 2>&1 &  pour deconnecter le programme.
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	alias ls='ls --color'
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	alias ls='ls -G'
-fi
-PATH=$HOME/.local/bin:$PATH
-
-source ~/.git-completion.bash
+source $HOME/Documents/GitHub/philconfig/post
--- install.sh
+++ install.sh
@@ -6,20 +6,26 @@ full_install()
 	rm $HOME/.bashrc
 	rm $HOME/.bash_profile
 	rm $HOME/.vimrc
+   rm -rf $HOME/.vim/colors
+   rm -rf $HOME/.vim/indent
 	rm -rf $HOME/Templates
 	rm -rf $HOME/.emacs.d
 	rm -rf $HOME/.config/sublime-text-3
 	rm $HOME/.gitconfig
 	rm $HOME/.git-completion.bash
+   rm $HOME/.git-prompt.sh
 
-	ln -s $installDir/sublime-text-3		$HOME/.config/sublime-text-3
-	ln -s $installDir/bashrc 				$HOME/.bashrc
-	ln -s $installDir/bash_profile 			$HOME/.bash_profile
-	ln -s $installDir/vimrc 				$HOME/.vimrc
-	ln -s $installDir/Templates 			$HOME/Templates
-	ln -s $installDir/emacs.d	 			$HOME/.emacs.d
-	ln -s $installDir/gitconfig 			$HOME/.gitconfig
-	ln -s $installDir/git-completion.bash 	$HOME/.git-completion.bash
+   ln-s$installDir/sublime-text-3           $HOME/.config/sublime-text-3
+   ln-s$installDir/bashrc                   $HOME/.bashrc
+   ln-s$installDir/bash_profile             $HOME/.bash_profile
+   ln-s$installDir/vimrc                    $HOME/.vimrc
+   ln-s$installDir/vim/colors               $HOME/.vim/colors
+   ln-s$installDir/vim/indent               $HOME/.vim/indent
+   ln-s$installDir/Templates                $HOME/Templates
+   ln-s$installDir/emacs.d                  $HOME/.emacs.d
+   ln-s$installDir/gitconfig                $HOME/.gitconfig
+   ln-s$installDir/git-completion.bash      $HOME/.git-completion.bash
+   ln-s$installDir/git-prompt.sh            $HOME/.git-prompt.sh
 
 	killall nautilus # For templates to take effect.
 
