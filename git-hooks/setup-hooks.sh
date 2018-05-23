4e8efac32d5e1a951dcd9976bfad46fdbdc65e80
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-05-23 17:54:38 +0000

    Changes to hooks

--- git-hooks/setup-hooks.sh
+++ git-hooks/setup-hooks.sh
@@ -2,9 +2,9 @@
 
 repo_dir=$(git rev-parse --show-toplevel)
 
-link=$repo_dir/.git/hooks/post-commit
+link_name=$repo_dir/.git/hooks/post-commit
 link_target=$repo_dir/git-hooks/post-commit
 
-rm -f $link
+rm -f $link_name
 
-ln -s $link_target $link
+ln -s $link_target $link_name
