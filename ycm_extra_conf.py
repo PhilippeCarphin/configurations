22895db17d76b39c015a9703616aa73c4361bb78
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-06-06 20:04:07 +0000

    CMC : Adapted ycm extra conf for CMC build

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -46,19 +46,20 @@ flags = [
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
 '-DUSE_CLANG_COMPLETER',
+'-DHAVE_ECBUFR'
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c99',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c++',
+'c',
 '-isystem',
 '../BoostParts',
 '-isystem',
@@ -71,6 +72,12 @@ flags = [
 '../llvm/tools/clang/include',
 '-I',
 '.',
+'-I',
+'/fs/cetus3/fs3/cmd/s/afsm/pca/ssm/workspace/libSPI_7.12.2_ubuntu-14.04-amd64-64/TCL/include',
+'-I',
+'/fs/cetus3/fs3/cmd/s/afsm/pca/ssm/workspace/libSPI_7.12.2_ubuntu-14.04-amd64-64/include',
+'-I',
+'/fs/cetus3/fs3/cmd/s/afsm/pca/ssm/workspace/eerUtils_3.3.1_ubuntu-14.04-amd64-64/include',
 '-I',
 './ClangCompleter',
 '-isystem',
