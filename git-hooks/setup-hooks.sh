36bc29e0499fa4ee4790e7a7d646a20541c2758f
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-07-21 02:45:23 -0400

    Made it so the setup hooks script doesn't depending on pwd when called

--- git-hooks/setup-hooks.sh
+++ git-hooks/setup-hooks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+my_dir=$(cd $(dirname $0) > /dev/null; pwd)
+
+cd $my_dir
 repo_dir=$(git rev-parse --show-toplevel)
 
 link_name=$repo_dir/.git/hooks/post-commit
 link_target=$repo_dir/git-hooks/post-commit
 
 rm -f $link_name
-
 ln -s $link_target $link_name
