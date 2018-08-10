dd31b8a05cad0a9eb381be1ffcb1865a2dd5765b
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-10 12:28:36 -0400

    Fixup after moving files : git-hooks has changed places

--- MISC/git-hooks/setup-hooks.sh
+++ MISC/git-hooks/setup-hooks.sh
@@ -6,7 +6,7 @@ cd $my_dir
 repo_dir=$(git rev-parse --show-toplevel)
 
 link_name=$repo_dir/.git/hooks/post-commit
-link_target=$repo_dir/git-hooks/post-commit
+link_target=$repo_dir/MISC/git-hooks/post-commit
 
 rm -f $link_name
 ln -s $link_target $link_name
