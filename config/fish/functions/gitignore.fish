a5806c4d184cd09547107208e72b97e933d0e110
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-12-23 14:41:59 -0500

    Added gitignore function

---
+++ config/fish/functions/gitignore.fish
@@ -0,0 +1,16 @@
+function gitignore
+
+    set -l git_root (git rev-parse --show-top-level)
+    set -l ignore_string ""
+
+    if [ "$argv[1]" = -d ]
+        set ignore_string $argv[2]
+        set gitignore_file (pwd)/.gitignore
+    else
+        set ignore_string $argv[1]
+        set gitignore_file $git_root/.gitignore
+    end
+
+    echo $ignore_string >> $gitignore_file
+    git add $gitignore_file
+end
