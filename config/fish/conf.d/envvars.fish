3d6cf958655f3a99c3bb2465a2e3b879ce92d0b4
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-07 17:56:55 -0500

    Removed setting of PATH in fish

--- config/fish/conf.d/envvars.fish
+++ config/fish/conf.d/envvars.fish
@@ -1,3 +1,4 @@
-set -x PATH $PATH $HOME/.local/bin
-set -x CDPATH $HOME . $HOME/Documents $HOME/Documents/GitHub
-set -x PHILCONFIG $HOME/.philconfig
+# Now I'm setting up all the environment in bash before the exec
+# set -x PATH $PATH $HOME/.local/bin
+# set -x CDPATH $HOME . $HOME/Documents $HOME/Documents/GitHub
+# set -x PHILCONFIG $HOME/.philconfig
