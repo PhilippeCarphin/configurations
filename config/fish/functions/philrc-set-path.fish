b36fdc4a6953d96b071a56a4d376abba636f9cb2
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-17 14:49:22 -0400

    Finalize path switching system for fish

---
+++ config/fish/functions/philrc-set-path.fish
@@ -0,0 +1,13 @@
+# Defined in /tmp/afsmpca/fish.iwJvMA/philrc-set-path.fish @ line 2
+function philrc-set-path
+	set -l option $argv[1]
+   switch $option
+      case all
+         set -x NEW_PATH (string split ':' $PHILRC_MY_PATH:$PHILRC_SYSTEM_PATH)
+      case phil
+         set -x NEW_PATH (string split ':' $PHILRC_MY_PATH)
+      case system
+         set -x NEW_PATH (string split ':' $PHILRC_SYSTEM_PATH)
+   end
+   set -x PATH $NEW_PATH
+end
