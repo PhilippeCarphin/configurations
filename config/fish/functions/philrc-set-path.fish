74c9fbc2b9d2c9e60963262887a0349494ba6d24
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-18 08:09:47 -0400

    Added same function for bash, +default case

--- config/fish/functions/philrc-set-path.fish
+++ config/fish/functions/philrc-set-path.fish
@@ -1,4 +1,4 @@
-# Defined in /Users/pcarphin/.config/fish/functions/philrc-set-path.fish @ line 2
+# Defined in /tmp/afsmpca/fish.cE5h9q/philrc-set-path.fish @ line 2
 function philrc-set-path
 	set -l option $argv[1]
    switch $option
@@ -8,6 +8,9 @@ function philrc-set-path
          set -x NEW_PATH (string split ':' $PHILRC_MY_PATH)
       case system
          set -x NEW_PATH (string split ':' $PHILRC_SYSTEM_PATH)
+      case '*'
+         echo Unknown option : $option
+         return 1
    end
    set -x PATH $NEW_PATH
 end
