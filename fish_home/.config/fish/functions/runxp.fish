c09d0b8e1849a3df55e2eb951bcd05b7f5caad31
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-01-13 15:58:54 -0500

    Change password hint after password reset for windows server

--- fish_home/.config/fish/functions/runxp.fish
+++ fish_home/.config/fish/functions/runxp.fish
@@ -1,4 +1,5 @@
+# Defined in /tmp/afsmpca/fish.fCaHqA/runxp.fish @ line 2
 function runxp
-    echo (tput setab 3)"Put a \"\$\" after password"(tput sgr 0)
-    nohup rdesktop -g 840x525 eccmcwts3 -d ECQUEBEC -u CarphinP > /dev/null &
+	echo (tput setab 3)"Put TWO \"\$\" after \"the\" password (even though it's two for the email account"(tput sgr 0)
+   nohup rdesktop -g 1900x1000 eccmcwts5 -d ECQUEBEC -u CarphinP > /dev/null &
 end
