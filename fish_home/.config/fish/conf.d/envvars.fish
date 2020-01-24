89cdb5b031d28da42095d4880de5c6b00c9e74d9
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-01-24 18:29:03 -0500

    Remove CDPATH from existence

--- fish_home/.config/fish/conf.d/envvars.fish
+++ fish_home/.config/fish/conf.d/envvars.fish
@@ -1,6 +1,5 @@
 # Now I'm setting up all the environment in bash before the exec
 # set -x PATH $PATH $HOME/.local/bin
-# set -x CDPATH $HOME . $HOME/Documents $HOME/Documents/GitHub
 # set -x PHILCONFIG $HOME/.philconfig
 set -x PHILRC_SYSTEM_PATH (string split ':' $PHILRC_SYSTEM_PATH)
 set -x PHILRC_MY_PATH (string split ':' $PHILRC_MY_PATH)
