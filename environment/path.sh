b51cbe252b8470539f864c9941047446eaf99bfc
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-11-16 09:50:51 -0500

    Basic fish and zsh setup

---
+++ environment/path.sh
@@ -0,0 +1,2 @@
+# PATH=${PATH:+${PATH}:}:$HOME/.local/bin
+export PATH=$HOME/.local/bin:$HOME/.philconfig/utils${PATH:+:${PATH}}
---
+++ fish_home/.config/fish/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/pcarphin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
