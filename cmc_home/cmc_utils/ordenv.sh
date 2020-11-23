1b5d257f9c2dc615d6cb1178fab77e910270709d
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-11-23 03:33:57 -0500

    Changed ordenv loading

---
+++ cmc_home/cmc_utils/ordenv.sh
@@ -0,0 +1,7 @@
+export ORDENV_SITE_PROFILE=20191220
+export ORDENV_COMM_PROFILE=eccc/20200409
+export ORDENV_GROUP_PROFILE=eccc/cmc/1.9.3
+. /fs/ssm/main/env/ordenv-boot-20200204.sh
+
+export EC_ATOMIC_PROFILE_VERSION=1.11.0
+. /fs/ssm/eccc/mrd/ordenv/profile/check_profile
--- cmc_home/cmc_utils/p.setup_spooki.def.sh
+++ cmc_home/cmc_utils/p.setup_spooki.def.sh
@@ -3,9 +3,9 @@ p.setup_spooki(){
     p.ordenv
     local setup_file=~/spooki-build/setup_spooki_dev_environment.sh
     if [ -f $setup_file ] ; then
-       _p.do_maybe_redirect source $setup_file
+        source $setup_file
     else
-       _p.do_maybe_redirect source ~/workspace/spooki/SETUP_ubuntu-18.04-amd64-64
+        source ~/workspace/spooki/SETUP_ubuntu-18.04-amd64-64
     fi
     alias spooki-dev='$SPOOKI_BIN_PATH/spooki_run'
 }
