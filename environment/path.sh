1b5ecde3fe751e9fedb7f91c7bb2f430cd56a00f
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-03 13:53:37 -0400

    Update path

--- environment/path.sh
+++ environment/path.sh
@@ -1,2 +1,2 @@
 # PATH=${PATH:+${PATH}:}:$HOME/.local/bin
-export PATH=$HOME/.local/bin:$HOME/.philconfig/utils${PATH:+:${PATH}}
+export PATH=$HOME/go/bin:$HOME/fs/bin:$HOME/.local/bin:$HOME/.philconfig/utils${PATH:+:${PATH}}
