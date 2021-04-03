e9c4ddfa76a7a6230978b3f93078ea854d6cd5bb
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-03 13:49:44 -0400

    Add LSCOLORS to config.fish

--- fish_home/.config/fish/config.fish
+++ fish_home/.config/fish/config.fish
@@ -1,6 +1,10 @@
 
+# From https://fish-users.narkive.com/qmgw3G37/ls-colors-with-fish
+set -Ux LSCOLORS Exfxcxdxbxegedabagacad
+# From https://apple.stackexchange.com/a/33679
+set -Ux LSCOLORS ExGxBxDxCxEgEdxbxgxcxd
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/pcarphin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# eval /Users/pcarphin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
