2a615dee056d41958a2ba3bd79961150ecca846a
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-03-16 16:14:06 -0400

    reorder path

--- environment/path.sh
+++ environment/path.sh
@@ -1,3 +1,3 @@
 # PATH=${PATH:+${PATH}:}:$HOME/.local/bin
-export PATH=$HOME/go/bin:$HOME/fs/bin:$HOME/.local/bin:$HOME/.philconfig/utils:${PATH:+:${PATH}}
 export PATH=/opt/homebrew/bin:${PATH:+:${PATH}}
+export PATH=$HOME/go/bin:$HOME/fs/bin:$HOME/.local/bin:$HOME/.philconfig/utils:${PATH:+:${PATH}}
