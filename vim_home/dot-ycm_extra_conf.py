e5dfb67d005a13f18a334b49b7c5e419ea69333e
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-08-16 18:21:26 -0400

    Updates

--- vim_home/dot-ycm_extra_conf.py
+++ vim_home/dot-ycm_extra_conf.py
@@ -35,8 +35,8 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-#'-std=c11',
-'-std=c++11',
+'-std=c11',
+# '-std=c++11',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
