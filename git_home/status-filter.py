6b630188e9eabde7b7ef923688737ae11f3bcce0
Philippe Carphin <phil103@hotmail.com>
2024-12-20 20:42:21 -0500

    Add a bunch of git aliases

--- git_home/status-filter.py
+++ git_home/status-filter.py
@@ -11,12 +11,11 @@ d = {
         "*": "1;37",                   # Header of individual submodule summary
         "  <": "31",                   # Missing commits in submodule
         "  >": "32",                   # Missing commits in submodule
-        "Untracked": "1;31"
+        "Untracked": "1;31"            # The title line of the untracked files section
 }
 
 import sys
 
-# Print all lines until we get to submodules
 for l in sys.stdin:
     for start, color in d.items():
         if l.startswith(start):
