5153e7d6c1f4775b4fb1e08334b9fe31ac464b8b
Philippe Carphin <phil103@hotmail.com>
2025-01-30 23:15:03 -0500

    bash install.sh links bashrc and bashprofile

--- bash_home/install.sh
+++ bash_home/install.sh
@@ -7,4 +7,7 @@ this_dir=$(cd $(dirname $0) && pwd)
 this_dir_rel=${this_dir#${HOME}/}
 
 ln -svf ${this_dir_rel}/dot-inputrc ~/.inputrc
+ln -svf ${this_dir_rel}/dot-bashrc ~/.bashrc
+ln -svf ${this_dir_rel}/dot-bash_profile ~/.bash_profile
+
 
